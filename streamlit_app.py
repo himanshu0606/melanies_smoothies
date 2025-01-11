bfa11e055a79208dcf25b83f7191a26f1233d9c9
HIMANSHU SARRAF <102588691+himanshu0606@users.noreply.github.com>
2025-01-11 09:02:22 +0530

    Update streamlit_app.py
    nutrition

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,7 @@ if ingredients_list:
 
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
+        st.subheader(fruit_chosen + 'Nutrition Information')
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         # st.text(smoothiefroot_response.json())
